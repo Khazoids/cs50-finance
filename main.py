6a7016d85e01e6ff92ff6d7d46c88b178ab7d374
Phillip Truong <97784787+Khazoids@users.noreply.github.com>
2022-09-25 12:21:13 -0700

    Revert

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL(os.environ.get("DATABASE_URL"))
+db = SQL(os.environ.get("sqlite:///finance.db"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
